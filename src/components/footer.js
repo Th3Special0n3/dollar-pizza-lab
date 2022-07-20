c98fd54a2d4664e304b7fb1ae080ba80a4e23e42
Th3Special0n3 <jaidenjoseph96@gmail.com>
2022-07-20 15:04:11 +0000

    dollar-pizza-lab

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './css/footer.css'
+
+
+function Footer() {
+  return(
+    <div className='Footer'>
+        <h1 className='header-text'> Contact Information</h1>
+      <h3 className='header-text'> Location: 555 Times Square, NY, NY 10000 </h3>
+      <h3 className='header-text'> Phone Number: 212 555 5555</h3>
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/menu.js
+++ src/components/menu.js
@@ -3,6 +3,9 @@ import Plain from './plain';
 import Pepperoni from './pepperoni';
 import MeatLover from './meat-lover';
 import Veggie from './veggie';
+import Hawaiian from './hawaiian';
+import Bbq from './bbq'
+
 import './css/menu.css';
 
 function Menu() {
@@ -15,6 +18,8 @@ function Menu() {
         <Pepperoni/>
         <MeatLover/>
         <Veggie/>
+        <Hawaiian/>
+        <Bbq/>
       </div>
     </div>
   );
